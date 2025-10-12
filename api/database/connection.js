9dafc70832c8fea618c715ed0cc872c643028ddb
DanBenedetti <danilobenedetti1@gmail.com>
2025-10-12 09:05:35 -0300

    ajustandos erros de conexão na vm

--- api/database/connection.js
+++ api/database/connection.js
@@ -1,9 +1,15 @@
-require('dotenv').config();
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize(process.env.DATABASE_URL, {
-  dialect: 'mysql',
-  logging: console.log, // Para ver os comandos SQL gerados
-});
+const sequelize = new Sequelize(
+  process.env.DB_DATABASE,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: 'mysql',
+    logging: console.log, // Para ver os comandos SQL gerados
+  }
+);
 
 module.exports = sequelize;
